d57674153e1959bf6acb3090826333aaadf0a5c5
daria-andrioaie <daria.andrioaie@gmail.com>
2022-07-18 15:53:00 +0300

    Intro in SwiftUI

---
+++ Intro_to_SwiftUI/Intro_to_SwiftUI/Intro_to_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Intro_to_SwiftUIApp.swift
+//  Intro_to_SwiftUI
+//
+//  Created by Daria Andrioaie on 18.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct Intro_to_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Daria Andrioaie on 18.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hi")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+        }
+    }
+}
