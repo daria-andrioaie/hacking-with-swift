e7c7fdc69418b6e875873336edac56e8474e6f39
daria-andrioaie <daria.andrioaie@gmail.com>
2022-07-21 10:24:35 +0300

    alerts, buttons, images

--- SwiftUI-prcatice-2/SwiftUI-prcatice-2/ContentView.swift
+++ SwiftUI-prcatice-2/SwiftUI-prcatice-2/ContentView.swift
@@ -8,8 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
+    
     var body: some View {
         Group {
+            // stacks
             HStack {
                 VStack {
                     Text("hi1")
@@ -27,28 +30,88 @@ struct ContentView: View {
                     Text("hi9")
                 }
             }
-        }
-        
-        // colors
-        ZStack {
-            VStack(spacing: 0) {
-                Color.red
-                Color.blue
+
+            // colors
+            ZStack {
+                VStack(spacing: 0) {
+                    Color.red
+                    Color.blue
+                }
+                // primary is the default color of the text
+                // .secondary is the secondary color of the text
+                //Color.primary
+                //Color(red: 1, green: 0.3, blue: 0.4)
+    //                .frame(minWidth: 200, maxWidth: .infinity, maxHeight: 200)
+                Text("Your content")
+                    .foregroundStyle(.secondary)
+                    .padding(50)
+                    .background(.ultraThinMaterial)
+
+                // if you want to ignore Apple's safe area, you have to add the "ignoreSafeArea modifier"
+                // DO NOT put important content in the safe area
             }
-            // primary is the default color of the text
-            // .secondary is the secondary color of the text
-            //Color.primary
-            //Color(red: 1, green: 0.3, blue: 0.4)
-//                .frame(minWidth: 200, maxWidth: .infinity, maxHeight: 200)
-            Text("Your content")
-                .foregroundStyle(.secondary)
-                .padding(50)
-                .background(.ultraThinMaterial)
+            .ignoresSafeArea()
+
+            // buttons
+            Group {
+                Button("Delete selection", role: .destructive, action: executeDelete)
+                VStack {
+                    Button("Button1") {
+
+                    }.buttonStyle(.bordered)
+                    Button("Button2") {
+
+                    }.buttonStyle(.borderedProminent)
+                        .tint(.mint)
+                    Button("Button3", role: .destructive) {
+
+                    }.buttonStyle(.bordered)
+                    Button("Button4", role: .destructive) {
+
+                    }.buttonStyle(.borderedProminent)
+                    Button {
+                        print("Button was tapped")
+                    } label: {
+                        Text("Tap me!")
+                            .padding()
+                            .foregroundColor(.white)
+                            .background(.red)
+                    }
+                }
+            }
+
+            // images
+            Group {
+                Image(systemName: "pencil")
+                Button {
+                    print("Pencil button")
+                } label: {
+                    // Image(systemName: "pencil").renderingMode(.original)
+                    // to keep the original color of the image, rather than Swift making it blue to show that they are tapable
+                    Label("Edit", systemImage: "pencil")
+                }
+            }
+
             
-            // if you want to ignore Apple's safe area, you have to add the "ignoreSafeArea modifier"
-            // DO NOT put important content in the safe area
+            // alert messages
+            // alerts are a function the state
+            Group {
+                Button("Show alert") {
+                    showingAlert = true
+                }
+                .alert("Important message", isPresented: $showingAlert) {
+                    // the button inside the alert will automatically dismiss the alert
+                    Button("Cancel", role: .cancel) { }
+                    Button("Delete", role: .destructive) { }
+                } message: {
+                    Text("Please read this")
+                }
+            }
         }
-        .ignoresSafeArea()
+    }
+    
+    func executeDelete() {
+        print("Now deleting")
     }
 }
 
