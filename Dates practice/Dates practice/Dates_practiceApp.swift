99524dd256d49d4ea92a9fc73fbc88e7f3fe8ce0
daria-andrioaie <daria.andrioaie@gmail.com>
2022-07-22 15:42:27 +0300

    practicing with dates in SwiftUI

---
+++ Dates practice/Dates practice/Dates_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dates_practiceApp.swift
+//  Dates practice
+//
+//  Created by Daria Andrioaie on 22.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct Dates_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
