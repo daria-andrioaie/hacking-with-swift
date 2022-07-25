0833414f59bd740a2a7d0f6c08005b446aaf8cd8
daria-andrioaie <daria.andrioaie@gmail.com>
2022-07-25 09:46:47 +0300

    started a new project using Create ML

---
+++ iSleep/iSleep/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  iSleep
+//
+//  Created by Daria Andrioaie on 22.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hi")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iSleep/iSleep/iSleepApp.swift
@@ -0,0 +1,17 @@
+//
+//  iSleepApp.swift
+//  iSleep
+//
+//  Created by Daria Andrioaie on 22.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct iSleepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
