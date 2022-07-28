1d061012dfa40abf5bc34010648bee1c2b02da74
daria-andrioaie <daria.andrioaie@gmail.com>
2022-07-28 12:55:54 +0300

    finished WordScramble mini app

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Daria Andrioaie on 26.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
